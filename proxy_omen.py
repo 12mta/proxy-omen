9e286654f6cd60e06debf625bd5fb1ec6bb37808
atm12 <andrewmeek12@gmail.com>
2019-06-16 10:36:05 -0400

    fixed the poc

--- proxy_omen.py
+++ proxy_omen.py
@@ -4,10 +4,8 @@ import json
 import re
 from time import sleep
 #import urllib2
-ptyp = "socks5"
+ptyp = "https"
 annon_level = "elite"
-working_ips = []
-
 class proxy:
     def __init__(self, proxy_type, ip_addr, proxy_port):
         self.proxy_type = proxy_type
@@ -21,34 +19,31 @@ user_agent = {'User-agent': 'Mozilla/5.0'}
 headers = {
     'User-agent': 'Mozilla/5.0'
 }
-def Main():
+def get_proxie(ptype, annon_levely):
+    api = "https://www.proxy-list.download/api/v1/get?type={}&anon={}".format(ptyp, annon_level)
+    r = requests.get(api, headers=headers)
+    proxy_content = r.text
+    #print(proxy_content)
+    ip_list = proxy_content.split()
+    return(ip_list)
 
-    def get_proxie(ptype, annon_levely):
-        api = "https://www.proxy-list.download/api/v1/get?type={}&anon={}".format(ptyp, annon_level)
-        r = requests.get(api, headers=headers)
-        proxy_content = r.text
-        #print(proxy_content)
-        ip_list = proxy_content.split()
-        return(ip_list)
-
-    ip_list = get_proxie(ptyp, annon_level)
-    def test_proxy():
-        number_of_ip = len(ip_list)
-        print("We are testing: {} ips".format(number_of_ip))
-        for ip in ip_list:
-            proxy_url = "{}://{}".format(ptyp, ip)
-            pre_proxie = {"https": proxy_url}
-            ddd
-            #print("tedsting: " + ip)
-
-            sleep(4)
-            try:
-                url = "https://httpbin.org/get'"
-                response = requests.get(url,proxies=pre_proxie)
-                print("{} is working ".format(ip))
-            except:
-                print("{} is dead, skiping".format(ip))
-            working_ips.append(ip)
-            #print(working_ips)
-    get_proxie(ptyp, annon_level)
-    test_proxy()
+ip_list = get_proxie(ptyp, annon_level)
+def test_proxy():
+    ip_num_untested = len(ip_list)
+    print("we are testinf {} proxies".format(ip_num_untested))
+    for ip in ip_list:
+        proxy_url = "{}://{}".format(ptyp, ip)
+        pre_proxie = {"https": proxy_url}
+        #print("testing: " + ip)
+        working_ips = []
+        try:
+            sleep(1)
+            url = "https://httpbin.org/ip'"
+            response = requests.get(url,proxies=pre_proxie)
+            print('{} is working'.format(ip))
+            working_ips.append()
+        except:
+            print("{} is dead, skiping".format(ip))
+get_proxie(ptyp, annon_level)
+test_proxy()
+print("don")
